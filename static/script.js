1461efea6482c3ec10a8e28a91841c3e643c7135
Thomas Birke <thomasbirke@gmx.de>
2021-04-18 13:33:04 +0200

    complete home

--- static/script.js
+++ static/script.js
@@ -1,45 +1,18 @@
-// const appDiv = document.querySelector('#app');
-// appDiv.innerHTML = '<p>Hello</p>'
-function clicked(message) {
-    console.log(message);
-}
-
-
-function repoUrl(project) {
-    return `https://github.com/EmbarkStudios/${project.name}`;
-}
-
-function stargazersUrl(project) {
-    return `https://github.com/EmbarkStudios/${project.name}/stargazers`;
-}
-
-function issuesUrl(project) {
-    return `https://github.com/EmbarkStudios/${project.name}/issues`;
-}
-
-function newsletterUrl(newsletter) {
-    return `${newsletter.link}`;
-}
-
-
-
-
-
 let state = {
     projects: [],
     newsletter: [],
     showSearch: false,
-    // search: ''
-}
+};
 
 init();
 
-function init() {
-    loadData();
+async function init() {
+    await loadProjectAndNewsletterData();
+    await loadGitHubData();
     handleSearchInput();
 }
 
-async function loadData() {
+async function loadProjectAndNewsletterData() {
     const data = await (await fetch('./data/data.json')).json();
     if (data.projects && data.newsletter) {
         data.projects = data.projects.filter((project) => !project.hidden);
@@ -47,7 +20,55 @@ async function loadData() {
         state.projects = data.projects.sort((a, b) => a.name.localeCompare(b.name));
         state.newsletter = data.newsletter;
     }
-    console.log(data);
+}
+
+async function loadGitHubData() {
+    try {
+        const gitHubResponse = await fetch("https://api.github.com/search/repositories?q=+org:EmbarkStudios+is:public&sort=created&order=asc&per_page=100");
+        if (gitHubResponse && gitHubResponse.ok) {
+            const { items: repos } = await gitHubResponse.json();
+
+            for (let i = 0; i < state.projects.length; i++) {
+                const project = state.projects[i];
+                const repo = repos.find((el) => el.name === project.name);
+                if (repo) {
+                    project.description = repo.description;
+                    project.stargazers_count = repo.stargazers_count;
+                    project.language = repo.language;
+                    project.forks_count = repo.forks_count;
+                    project.open_issues_count = repo.open_issues_count;
+                }
+            }
+            hydrateHtmlWithGitHubData();
+        }
+
+    } catch (error) {
+        console.error('Failed to get repos info: ' + error);
+    }
+}
+
+function hydrateHtmlWithGitHubData() {
+    for (let i = 0; i < state.projects.length; i++) {
+        const project = state.projects[i];
+        updateHtml(project.name, 'star-count-', project.stargazers_count);
+        updateHtml(project.name, 'issue-count-', project.open_issues_count);
+        updateHtml(project.name, 'project-description-', project.description);
+    }
+}
+
+function updateHtml(projectName, cssClass, innerHtml) {
+    const htmlTags = document.body.querySelectorAll('.' + cssClass + projectName);
+    if (htmlTags) {
+        if (innerHtml !== undefined) { // add innerHTML if given
+            for (let j = 0; j < htmlTags.length; j++) {
+                htmlTags[j].innerHTML = innerHtml;
+            }
+        } else { // remove HTML if no innerHtml value
+            for (let j = 0; j < htmlTags.length; j++) {
+                htmlTags[j].parentNode.removeChild(htmlTags[j]);
+            }
+        }
+    }
 }
 
 // search start
@@ -89,17 +110,16 @@ function handleSearchInput() {
 
 function showAllSearchedProjects(searchedProjects) {
     for (let i = 0; i < state.projects.length; i++) {
-        // instead of template-strings to be able to support IE
+        //normal string concat instead of template-strings to be able to support IE
         const card = document.body.querySelector('#search-project-card-' + state.projects[i].name);
         if (!card) return;
 
         const isInSearchedProjects = searchedProjects.some((p) => p.name.includes(state.projects[i].name));
-        if(isInSearchedProjects) {
+        if (isInSearchedProjects) {
             card.classList.remove('hidden');
         } else {
             card.classList.add('hidden');
         }
     }
 }
-
 // search end
